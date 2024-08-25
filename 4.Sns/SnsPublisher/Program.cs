d0f47ac72761e7d725a630edb7a0978de1bc6f74
Keoma Kevin Baudin <keoma4@gmail.com>
2024-08-25 20:13:40 -0300

    add SNS message publisher

---
+++ 4.Sns/SnsPublisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using SnsPublisher;
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    FullName = "Keom Baudin",
+    Email = "keoma@keomabaudin.com",
+    GitHubUsername = "kafziel4",
+    DateOfBirth = new DateTime(1991, 1, 1)
+};
+
+var snsClient = new AmazonSimpleNotificationServiceClient();
+
+var topicArnResponse = await snsClient.FindTopicAsync("customers");
+
+var publishRequest = new PublishRequest
+{
+    TopicArn = topicArnResponse.TopicArn,
+    Message = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await snsClient.PublishAsync(publishRequest);
