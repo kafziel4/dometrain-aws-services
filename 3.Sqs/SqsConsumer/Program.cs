b7449731fe27beb95d8ef1fee088ce06a8339b88
Keoma Kevin Baudin <keoma4@gmail.com>
2024-08-25 15:19:53 -0300

    add SQS message consumer

---
+++ 3.Sqs/SqsConsumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var cts = new CancellationTokenSource();
+var sqsClient = new AmazonSQSClient();
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageSystemAttributeNames = ["All"],
+    MessageAttributeNames = ["All"]
+};
+
+while (!cts.IsCancellationRequested)
+{
+    var response = await sqsClient.ReceiveMessageAsync(receiveMessageRequest, cts.Token);
+
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine($"Message Id: {message.MessageId}");
+        Console.WriteLine($"Message Body: {message.Body}");
+
+        await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle);
+    }
+
+    await Task.Delay(3000);
+}
