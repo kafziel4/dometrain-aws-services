71f66d33440df49c6daaa41cb66ba3ca7e510c28
Keoma Kevin Baudin <keoma4@gmail.com>
2024-08-26 01:23:09 -0300

    add Secrets Manager app

---
+++ 7.SecretsManager/SecretsManager.App/Program.cs
@@ -0,0 +1,33 @@
+﻿using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+var secretsManagerClient = new AmazonSecretsManagerClient();
+
+var listSecretVersionsRequest = new ListSecretVersionIdsRequest
+{
+    SecretId = "ApiKey",
+    IncludeDeprecated = true
+};
+
+var versionResponse = await secretsManagerClient.ListSecretVersionIdsAsync(listSecretVersionsRequest);
+
+var request = new GetSecretValueRequest
+{
+    SecretId = "ApiKey",
+    // VersionStage = "AWSPREVIOUS"
+    // VersionStage = "AWSCURRENT"
+    VersionId = "3611dede-5fbc-4a0b-949a-585f3c6f84b2"
+};
+
+var response = await secretsManagerClient.GetSecretValueAsync(request);
+
+Console.WriteLine(response.SecretString);
+
+var describeSecretRequest = new DescribeSecretRequest
+{
+    SecretId = "ApiKey"
+};
+
+var describeResponse = await secretsManagerClient.DescribeSecretAsync(describeSecretRequest);
+
+Console.WriteLine();
